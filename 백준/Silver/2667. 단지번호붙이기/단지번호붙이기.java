ca04cc34e93fb51b61400bfb0b79dc7e42593d3c
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2024-06-18 11:51:16 +0900

    [Silver I] Title: 단지번호붙이기, Time: 144 ms, Memory: 14720 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -3,12 +3,13 @@ import java.io.*;
 
 public class Main {
 	
-	static int[][] danji;
-	static boolean[][] visited;
+	static int[][] map; // 지도 정보
+	static boolean[][] visited; // 특정 위치 방문 했는지
 	static int[] dx = {0,0,-1,1};
 	static int[] dy = {-1,1,0,0};
-	static List<Integer> result;
-	static int cnt, N;
+	static List<Integer> result; // 각 단지에 속한 아파트 수 저장
+	static int cnt; // 현재 단지에 속한 아파트 수를 세기 위한 변수
+	static int N; // 지도의 크기
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,45 +17,47 @@ public class Main {
 		
 		result = new LinkedList<>(); 
 		N = Integer.parseInt(br.readLine());
-		danji = new int[N][N];
-		visited = new boolean[N][N]; //단지에 속해있는지
-		cnt = 1; //기준이 아파트(단지로 묶일 첫 아파트)가 포함될 때니 1로 초기화
+		map = new int[N][N];
+		visited = new boolean[N][N]; 
+		cnt = 1;
 		
 		for(int i=0;i<N;i++) {
 			String str = br.readLine();
 			for(int j=0;j<N;j++) {
-				danji[i][j] = str.charAt(j)-'0';
+				map[i][j] = str.charAt(j)-'0';
 			}
 		}
+		// 각 줄의 문자열을 읽어서 각 문자를 숫자로 변환해 배열에 저장
 		
 		
 		for(int x=0;x<N;x++) {
 			for(int y=0;y<N;y++) {
-				if(danji[x][y]==1 && !visited[x][y]) {
-					dfs(x,y);
-					result.add(cnt);
-					cnt = 1;
+				if(map[x][y]==1 && !visited[x][y]) { // 아파트가 있고 아직 방문하지 않은 지점 찾기
+					dfs(x,y); // 지점 발견하면 메서드 호출
+					result.add(cnt); // 탐색 끝나면 cnt 값을 결과 리스트에 추가
+					cnt = 1; // cnt 초기화
 				}
 			}
 		}
 		
-		Collections.sort(result);
+		Collections.sort(result); // 정렬
 		
-		bw.write(result.size()+"\n");
-		for(int r : result) bw.write(r+"\n");
+		bw.write(result.size()+"\n"); // 총 단지 수
+		for(int r : result) bw.write(r+"\n"); // 각 단지에 속한 아파트 수
 		bw.flush();
 		bw.close();
 
 	}
 	
 	public static void dfs(int x, int y) {
-		visited[x][y] = true;
+		visited[x][y] = true; // 위치 방문
 		
-		for(int i=0;i<4;i++) {
+		for(int i=0;i<4;i++) { // 상하좌우 네 방향으로 이동하며 이동할 수 있는 지 검사
 			int nx = dx[i]+x;
 			int ny = dy[i]+y;
 			
-			if(nx>=0 && ny>=0 && nx<N && ny<N && !visited[nx][ny] && danji[nx][ny]==1) {
+			if(nx>=0 && ny>=0 && nx<N && ny<N && !visited[nx][ny] && map[nx][ny]==1) {
+				// 이동할 수 있는 경우
 				cnt++;
 				dfs(nx,ny);
 			}
