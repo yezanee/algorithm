c6d12a8b64ea0c282668f616235d93c536641ad7
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2024-07-20 23:47:28 +0900

    [Silver III] Title: 거북이, Time: 452 ms, Memory: 24140 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.java
+++ 백준/Silver/8911. 거북이/거북이.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main
 {
-	public static void main(String[] args) throws NumberFormatException, IOException{
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
