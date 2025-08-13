19536e81a26d74bdadd632446cdb0c1c55150060
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2025-08-13 14:59:27 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.53 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int max = 0;
+        
+        Arrays.sort(numbers);
+        
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i+1; j < numbers.length; j++) {
+                if (numbers[i] * numbers[j] > max) {
+                    max = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return max;
+    }
+}
