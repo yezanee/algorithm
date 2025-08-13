22a2c5a7ead61cde24d7d23b07b558af84f0b067
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2025-08-13 14:45:43 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 11.52 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i = my_string.length() - 1; i >= 0; i--) {
+            answer += my_string.charAt(i);
+        }
+        
+        return answer;
+    }
+}
