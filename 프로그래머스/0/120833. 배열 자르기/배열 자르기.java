1634d1108d2f4532392eeed6cc49351c5eb80dc5
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2025-08-14 16:18:27 +0900

    [level 0] Title: 배열 자르기, Time: 2.42 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = num1; i <= num2; i++) {
+            list.add(numbers[i]);
+        }
+        
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
+}
