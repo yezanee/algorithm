54e39e14b26378509cd7c203af8296e83908bc2c
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-02-04 18:16:53 +0900

    [Silver II] Title: 스택 수열, Time: 252 ms, Memory: 28004 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,33 +1,43 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       int N = Integer.parseInt(br.readLine());
-       ArrayList<Integer> list = new ArrayList<>();
-       StringBuilder sb = new StringBuilder();
-       
-       int current = 1;  // 다음에 push할 숫자
-       for (int i = 0; i < N; i++) {
-           int num = Integer.parseInt(br.readLine());
-           
-           // 현재까지 숫자(current)부터 목표 숫자(num)까지 push
-           while (current <= num) {
-               list.add(current++);
-               sb.append("+\n");
-           }
-           
-           // 마지막에 들어간 숫자가 num과 같다면 pop
-           if (!list.isEmpty() && list.get(list.size() - 1) == num) {
-               list.remove(list.size() - 1);
-               sb.append("-\n");
-           } else {
-               // 원하는 수열을 만들 수 없는 경우
-               System.out.println("NO");
-               return;
-           }
-       }
-       System.out.print(sb);
-    }
-}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] numArr = new int[n];
+		Stack<Integer> stack = new Stack<>();
+		int num = 1;
+		boolean result = true;
+		
+		for(int i=0; i<n; i++) {
+			numArr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(numArr[i] >= num) { // 현재 수열 값 >= 오름차순 자연수
+				while (numArr[i] >= num) { // 값이 같아질 때까지 push
+					stack.push(num++);
+					sb.append("+\n");
+				}
+				stack.pop(); // 값이 같아질 때
+				sb.append("-\n");
+			}
+			else { // 현재 수열 값 < 오름차순 자연수
+				int j = stack.pop(); // pop을 수행해 수열 원소를 꺼냄
+				// 스택의 가장 위의 수가 만들어야 하는 수열의 수보다 크면 수열 출력 X
+				if (j > numArr[i]) {
+					System.out.println("NO");
+					result = false;
+					break;
+				}
+				else {
+					sb.append("-\n");
+				}
+			}
+		}
+		if (result) System.out.println(sb.toString());
+	}
+}					
