154d92f51c7bd16b14622faf897a82acdc1d8381
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-02-05 17:47:33 +0900

    [Bronze II] Title: 수 정렬하기, Time: 132 ms, Memory: 16136 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,27 +1,25 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        int[] num = new int[N];
-        
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            num[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        Arrays.sort(num);
-        
-        for(int i=0; i<N; i++) {
-            bw.write(num[i] + "\n");
-        }
-        
-        bw.flush();
-        bw.close();
-        
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(int i=0; i<N; i++) {
+			bw.write(arr[i]+"\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
 }
