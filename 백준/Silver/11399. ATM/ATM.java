ae46d34391878f94eaeee4459cefe7bd48719e56
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-02-05 20:36:13 +0900

    [Silver IV] Title: ATM, Time: 96 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,29 +1,27 @@
 import java.util.*;
+import java.io.*;
 
-public class Main
-{
-	public static void main(String[] args) {
-	    
-	    Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); // 사람의 수
-		int[] Pi = new int[N]; // 각 사람이 돈을 인출하는데 걸리는 시간
-		int result = 0;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // 사람 수
+		int[] P = new int[N]; // 사람들이 돈을 인출하는데 걸리는 시간
 		
-		for(int i = 0; i < N; i++) {
-		    Pi[i] = sc.nextInt();
-		}
-		// 여기까지 입력 받고 초기화
-		
-		// 그리디 알고리즘
-		Arrays.sort(Pi); // 시간 배열 오름차순 정렬
-		
-		int sum = 0;
-		for (int i = 0; i < N; i++) {
-		    sum += Pi[i];
-		    result += sum;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			P[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		System.out.println(result); // 결과 출력
+		Arrays.sort(P);
 		
+		
+		int sum = P[0];
+		int current = P[0];
+		for(int i=1; i<N; i++) {
+			current += P[i];
+			sum += current;
+		}
+		
+		System.out.println(sum);
 	}
 }
