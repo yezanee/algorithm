96e136fa3278365311bac5b386180774c7aa7325
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-02-22 01:38:10 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 952 ms, Memory: 121576 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,70 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
-        int[] nums = new int[N];
-        
-        for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        Arrays.sort(nums);
-        
-        for(int i = 0; i < N; i++) {
-            bw.write(nums[i] + "\n");
-        }
-        
-        bw.flush();
-        bw.close();
-    }
+public class Main {
+	public static int[] A, tmp;
+	public static long result;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		
+		A = new int[N+1]; // 1-based
+		tmp = new int[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+		}
+		
+		merge_sort(1, N);
+		
+		for(int i=1; i<=N; i++) {
+			bw.write(A[i] + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	private static void merge_sort(int s, int e) {
+		if (e-s < 1) return; // 구간 길이가 1 이하 = 이미 정렬된 상태 (s >= e)
+		int m = s + (e-s) / 2; // 정수 오버플로우 방지
+		
+		merge_sort(s, m);
+		merge_sort(m+1, e);
+		
+		for(int i=s; i<=e; i++) {
+			tmp[i] = A[i];
+		}
+		
+		int k = s;
+		int index1 = s;
+		int index2 = m + 1;
+		while (index1 <= m && index2 <= e) {
+			// 양쪽 그룹의 index가 가리키는 값을 비교해 더 작은 수를 선택해 배열에 저장하고,
+      // 선택된 데이터의 index 값을 오른쪽으로 한 칸 이동하기
+			if (tmp[index1] > tmp[index2]) {
+			  A[k] = tmp[index2];
+	      k++;
+        index2++;
+      } else {
+        A[k] = tmp[index1];
+        k++;
+	      index1++;
+      }
+		}
+		
+		while (index1 <= m) { // 한쪽 그룹이 모두 선택된 후 남아있는 값 제거하기
+			A[k] = tmp[index1];
+			k++;
+			index1++;
+		}
+		
+		while(index2 < e) {
+			A[k] = tmp[index2];
+			k++;
+			index2++;
+		}
+	}
 }
