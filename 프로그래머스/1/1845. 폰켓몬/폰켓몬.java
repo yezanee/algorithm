21bccf69fb42ed0480e94b8a0d121b6d85b35cc8
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-03-06 01:20:17 +0900

    [level 1] Title: 폰켓몬, Time: 5.88 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int N = (nums.length) / 2; // 가져갈 수 있는 폰켓몬 수
+        
+        int N = nums.length / 2;
         HashSet<Integer> set = new HashSet<>();
         
         for(int i : nums) {
