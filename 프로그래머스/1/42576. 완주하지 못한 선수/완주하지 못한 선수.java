3912fad3d6560f95329d3a8b0691122e3fb01d51
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-03-06 01:16:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.05 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,24 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String result = "";
+        String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
         
-        for(int i=0; i<participant.length; i++) {
-            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        for(String s : participant) {
+            map.put(s, (map.getOrDefault(s, 0) + 1));
         }
         
-        for(int i=0; i<completion.length; i++) {
-            map.put(completion[i], map.getOrDefault(completion[i], 0) - 1);
+        for(String s : completion) {
+            map.put(s, (map.get(s) - 1));
         }
         
-        for(int i=0; i<participant.length; i++) {
-            if(map.get(participant[i]) > 0) {
-                result = participant[i];
-                break;
-            }
+        for(String s : map.keySet()) {
+            if(map.get(s) > 0) return s;
         }
-        
-        return result;
+        return answer;
     }
 }
