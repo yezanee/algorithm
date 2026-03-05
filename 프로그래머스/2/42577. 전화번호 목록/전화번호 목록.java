81be4231bf8edd503f2201b57dc272cbc2eb6136
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-03-06 01:51:00 +0900

    [level 2] Title: 전화번호 목록, Time: 344.04 ms, Memory: 251 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        boolean answer = true;
+        Set<String> set = new HashSet<>(Arrays.asList(phone_book));
         
-        for(int i = 0; i < phone_book.length - 1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
+        for(String s : phone_book) {
+            for(int i=1; i<s.length(); i++) {
+                if(set.contains(s.substring(0, i))) return false;
             }
         }
-        
-        return true;
+        return answer;
     }
 }
