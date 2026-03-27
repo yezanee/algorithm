cdc945eb2c92c237d1999ae0ae2e4f1638c6096e
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-03-27 15:04:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.33 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,18 +1,15 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        List<Integer> list = new ArrayList<>();
+    public Stack<Integer> solution(int []arr) {
         
-        list.add(arr[0]);
+        Stack<Integer> stack = new Stack<>();
         
-        for(int i=1; i<arr.length; i++) {
-            if(arr[i-1] == arr[i]) continue;
-            list.add(arr[i]);
+        for(int i : arr) {
+            if(!stack.isEmpty() && i == stack.peek()) continue;
+            stack.push(i);
         }
 
-        return list.stream()
-                    .mapToInt(i -> i)
-                    .toArray();
+        return stack;
     }
 }
