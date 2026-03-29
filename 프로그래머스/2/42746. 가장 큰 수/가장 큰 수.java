984922f977587237ff253165edffdff87843c0d8
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-03-29 17:09:41 +0900

    [level 2] Title: 가장 큰 수, Time: 217.65 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,25 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] intToString = new String[numbers.length];
-        
-        // 1. int → String 변환
+        String[] arr = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            intToString[i] = String.valueOf(numbers[i]);
+            arr[i] = String.valueOf(numbers[i]);
         }
-        
-        // 2. 정렬
-        Arrays.sort(intToString, (a, b) -> (b + a).compareTo(a + b));
-        
-         // 3. 가장 큰 수가 0이면 (즉, 전부 0이면) 0 반환
-        if (intToString[0].equals("0")) return "0";
-        
-        // 4. 이어 붙이기
-        StringBuilder sb = new StringBuilder();
-        for (String s : intToString) {
-            sb.append(s);
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        StringBuilder answer = new StringBuilder();
+        for (String s : arr) {
+            answer.append(s);
         }
-        
-        return sb.toString();
+
+        if (answer.charAt(0) == '0') return "0";
+
+        return answer.toString();
     }
 }
