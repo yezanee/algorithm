128a65885577ed062c1d4e5acee70ea9d053c26f
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-04-01 19:43:14 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,16 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        HashMap<String, Integer> map = new HashMap<>();
         
-        for(String[] c : clothes) {
-            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+        HashMap<String, Integer> comb = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++) {
+            comb.put(clothes[i][1], comb.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        for(int v : map.values()) {
-            answer *= (v + 1); // 안 입는 경우의 수도 포함
+        // 핵심 ...
+        for(String type : comb.keySet()) {
+            answer *= (comb.get(type) + 1); // 입는 경우, 안 입는 경우
         }
         
-        return answer - 1; // 아무것도 안 입는 경우 빼기
+        return answer - 1; // 아무것도 안 입는 경우 제외
     }
 }
