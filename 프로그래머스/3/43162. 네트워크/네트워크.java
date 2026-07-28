e56baf4c867dc6fecf8d757e4f01f4030bddf6c0
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-07-28 09:45:22 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,13 +1,11 @@
 class Solution {
-    boolean[] visited;
-    
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
         int answer = 0;
+        boolean[] visited = new boolean[n];
         
-        for(int i=0; i<n; i++) {        
-            if(!visited[i] && computers[i][i] == 1) {
-                dfs(i, computers);
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
+                dfs(n, computers, i, visited);
                 answer++;
             }
         }
@@ -15,12 +13,12 @@ class Solution {
         return answer;
     }
     
-    public void dfs(int start, int[][] computers) {
-        visited[start] = true;
+    public void dfs(int n, int[][] computers, int cur, boolean[] visited) {
+        visited[cur] = true;
         
-        for(int i=0; i<computers.length; i++) {
-            if(!visited[i] && computers[start][i] == 1) {
-                dfs(i, computers);
+        for(int i=0; i<n; i++) {
+            if(!visited[i] && cur != i && computers[cur][i] == 1) {
+                dfs(n, computers, i, visited);
             }
         }
     }
