d9db41e908b1ec23e7e6e2b5dc4d631e2aa7e7d6
Yoon Yejin <124289103+yezanee@users.noreply.github.com>
2026-07-29 14:56:59 +0900

    [level 3] Title: 단어 변환, Time: 0.37 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,48 +1,43 @@
 import java.util.*;
 
-class Node {
-    String word;
-    int count;
-    
-    Node(String word, int count) {
-        this.word = word;
-        this.count = count;
-    }
-}
-
 class Solution {
     public int solution(String begin, String target, String[] words) {
+        Queue<String> queue = new LinkedList<>();
         boolean[] visited = new boolean[words.length];
-        Queue<Node> queue = new LinkedList<>();
+        int changeCnt = 0;
         
-        queue.offer(new Node(begin, 0));
+        queue.add(begin);
         
         while(!queue.isEmpty()) {
-            Node now = queue.poll();
+            int size = queue.size();
             
-            if(now.word.equals(target)) return now.count;
-            
-            for(int i=0; i<words.length; i++) {
-                if(!visited[i] && canChange(now.word, words[i])) {
-                    queue.offer(new Node(words[i], now.count + 1));
-                    visited[i] = true;
+            for(int k=0; k<size; k++) {
+                String cur = queue.poll();
+                
+                if (cur.equals(target)) {
+                    return changeCnt;
+                }
+                
+                for(int i=0; i<words.length; i++) {
+                    if (visited[i]) continue;
+                    
+                     String word = words[i];
+                    
+                    int cnt = 0;
+                    for (int j = 0; j < word.length(); j++) {
+                        if (word.charAt(j) == cur.charAt(j)) {
+                            cnt++;
+                        }
+                    }
+                    
+                    if (cnt == word.length() - 1) {
+                        visited[i] = true;
+                        queue.add(word);
+                    }
                 }
             }
-            
-            
+            changeCnt++;
         }
-        
         return 0;
-    }
-    
-    public boolean canChange(String s1, String s2) {
-        int cnt = 0;
-        
-        for(int i = 0; i < s1.length(); i++) {
-            if(!(s1.charAt(i) == s2.charAt(i))) {
-                cnt++;
-            }
-        }
-        return cnt == 1;
     }
 }
